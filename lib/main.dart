78c79bc69c6f70133bf3c4271ac065de4c904960
xuantruong1106 <truongnx.21it@vku.udn.vn>
2025-01-06 20:56:00 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -2,95 +2,57 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  runApp(Magic8BallApp());
+  runApp(const DiceApp());
 }
 
-class Magic8BallApp extends StatelessWidget {
-  const Magic8BallApp({super.key});
+class DiceApp extends StatelessWidget {
+  const DiceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Magic8Ball(),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: const Text('Dicee'),
+          ),
+          body: const DicePage(),
+          backgroundColor: Colors.black12,
+        ),
+      ),
     );
   }
 }
 
-class Magic8Ball extends StatefulWidget {
-  const Magic8Ball({super.key});
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
 
   @override
-  _Magic8BallState createState() => _Magic8BallState();
+  _DicePageState createState() => _DicePageState();
 }
 
-class _Magic8BallState extends State<Magic8Ball> {
-  final List<String> responses = [
-    "Yes, definitely.",
-    "Ask again later.",
-    "It is certain.",
-    "Don't count on it.",
-    "My sources say no.",
-    "Outlook good.",
-    "Very doubtful.",
-    "Without a doubt.",
-    "Better not tell you now.",
-    "Yes.",
-    "No.",
-    "Signs point to yes.",
-    "Concentrate and ask again.",
-    "Reply hazy, try again."
-  ];
+class _DicePageState extends State<DicePage> {
+  int diceNumber = 1;
 
-  String currentResponse = "Ask a question!";
-
-  void getRandomResponse() {
-    final random = Random();
+  void rollDice() {
     setState(() {
-      currentResponse = responses[random.nextInt(responses.length)];
+      diceNumber = Random().nextInt(6) + 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Magic 8 Ball"),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
-      body: Container(
-        color: Colors.grey[900],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              currentResponse,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 50.0),
-            ElevatedButton(
-              onPressed: getRandomResponse,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
-              ),
-              child: Text(
-                "Ask the Magic 8 Ball",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextButton(
+            onPressed: rollDice,
+            child: Image.asset('../images/dice$diceNumber.png'),
+          ),
+        ],
       ),
     );
   }
